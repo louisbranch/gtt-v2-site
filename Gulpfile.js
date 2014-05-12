df170b5a6b196b0da8ba5980308a8fadd5c80444
Luiz Branco <me@luizbranco.com>
2014-05-12 19:21:00 -0300

    Add scafolding structure

--- Gulpfile.js
+++ Gulpfile.js
@@ -11,12 +11,12 @@ gulp.task("test", function () {
 });
 
 gulp.task("lint", function () {
-  gulp.src(["app.js", "api/**/*.js"])
+  gulp.src(["app.js", "client/**/*.js"])
     .pipe(jshint());
 });
 
 gulp.task("default", function () {
-  gulp.watch(["app.js", "api/**", "test/**"], function() {
+  gulp.watch(["app.js", "client/**", "test/**"], function() {
     gulp.run("test");
   });
 });
--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 var koa = require("koa");
 var serve = require("koa-static");
+var mount = require("koa-mount");
 var router = require("koa-trie-router");
 var app = module.exports = koa();
 
 // Middlewares ----------------------------- //
 
 app.use(router(app));
-app.use(serve("public"));
+app.use(serve("build"));
+app.use(mount("/client", serve("client")));
 
 // Routing --------------------------------- //
 // TODO
