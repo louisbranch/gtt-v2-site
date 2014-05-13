5917ba51b09c373c4d06f338fdb6e6270bb5b49b
Luiz Branco <me@luizbranco.com>
2014-05-12 23:32:47 -0300

    Fix jquery dependency on boot

--- client/boot/index.js
+++ client/boot/index.js
@@ -1,1 +1,10 @@
-window.console.log("hiii");
+var $ = require("jquery");
+var Backbone = require("backbone");
+var Marionette = require("backbone.marionette");
+var HeaderView = require("header");
+
+Backbone.$ = Marionette.$ = $; // Fix for missing dependency
+
+var header = new HeaderView();
+
+$("body").append(header.render().el);
---
+++ client/header/index.js
@@ -0,0 +1,6 @@
+var Backbone = require("backbone.marionette");
+
+module.exports = Backbone.ItemView.extend({
+  tagName: "header",
+  template: "#template-header"
+});
