382046ac07bb01f331f69eb9b011f7d8090c4875
Luiz Branco <me@luizbranco.com>
2014-05-18 13:30:56 -0300

    Simplify auth policy

--- server/controllers/dashboard.js
+++ server/controllers/dashboard.js
@@ -1,5 +1,12 @@
+var request = require("../helpers/request");
+
 module.exports = function *() {
+  var email = this.session.email;
+  var token = this.session.token;
+  var url = "/projects?email=" + email + "&token=" + token;
+  var projects = yield request("get", url, this);
+
   yield this.render('dashboard', {
-    projects: this.projects
+    projects: projects
   });
 }
--- server/policies/authenticate.js
+++ server/policies/authenticate.js
@@ -1,14 +1,7 @@
-var request = require("../helpers/request");
-
 module.exports = function *(next) {
-  var email, token, url;
-  email = this.session.email;
-  token = this.session.token;
-  if (email && token) {
-    url = "/projects?email=" + email + "&token=" + token;
-    this.projects = yield request("get", url, this);
-    yield next;
-  } else {
+  if (!this.session.email || !this.session.token) {
     yield this.render('index');
+  } else {
+    yield next;
   }
 };
