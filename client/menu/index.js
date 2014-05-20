65ea4d50f3fdfec39d6faaefebe82a5323acb6f2
Luiz Branco <me@luizbranco.com>
2014-05-19 23:24:51 -0300

    Add header icon color

--- client/menu/index.js
+++ client/menu/index.js
@@ -4,24 +4,27 @@ module.exports = Marionette.ItemView.extend({
   template: "#template-menu",
 
   templateHelpers: {
+
     projects: function () {
       return window.GTT.projects;
+    },
+
+    color: function (name) {
+      var hash = 0;
+      var color = '#';
+
+      for (var i = 0; i < name.length; i++) {
+        hash = name.charCodeAt(i) + ((hash << 5) - hash);
+      }
+
+      for (var j = 0; j < 3; j++) {
+        var value = (hash >> (j * 8)) & 0xFF;
+        color += ('00' + value.toString(16)).substr(-2);
+      }
+
+      return color;
     }
+
   }
 
 });
-
-function stringToColor(str) {
-  var hash = 0;
-  var color = '#';
-
-  for (var i = 0; i < str.length; i++) {
-    hash = str.charCodeAt(i) + ((hash << 5) - hash);
-  }
-
-  for (var i = 0; i < 3; i++) {
-    var value = (hash >> (i * 8)) & 0xFF;
-    color += ('00' + value.toString(16)).substr(-2);
-  }
-  return color;
-}
