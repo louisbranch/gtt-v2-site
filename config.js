95ad832d876e25170e78b55f8838bec8159274c4
Luiz Branco <me@luizbranco.com>
2014-11-02 10:49:43 -0500

    Update code to prod

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var env = process.env;
 
 module.exports = {
-  server: env.SERVER_URL || "http://localhost:8080/v1",
+  server: env.SERVER_URL || "http://curry.io/v1",
   secret: env.SESSION_SECRET || "secret"
 };
